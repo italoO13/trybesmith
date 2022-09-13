d1f62a645b8eb189ab4d83bdbcaf772c12c4da49
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-13 19:17:16 -0300

    feat: add rota para cadastro de novos usuarios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import ProductsRouter from './products.route';
+import UsersRouter from './users.route';
 
 const router = Router();
 
 router.use('/products', ProductsRouter);
+router.use('/users', UsersRouter);
 
 export default router;
