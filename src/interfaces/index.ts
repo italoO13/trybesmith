3017fe18b52bb2854c142853799ad0d153bcc06e
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-13 19:16:50 -0300

    feat: interface de user

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,6 +5,14 @@ export interface Product {
   orderId?: number
 }
 
+export interface User {
+  id?:number, 
+  username: string,
+  classe: string,
+  level: string,
+  password: string,
+}
+
 export interface Order {
   id?:number, 
   userId:number,
